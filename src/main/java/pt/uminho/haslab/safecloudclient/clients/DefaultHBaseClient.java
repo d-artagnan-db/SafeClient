b7b48aad8f1d50b30cbd2f58d0156a10476564d1
ricardomacedo13 <ricardomacedo13@gmail.com>
2017-03-01 16:19:28 +0000

    Padding added to Standard Encryption

--- src/main/java/pt/uminho/haslab/safecloudclient/clients/DefaultHBaseClient.java
+++ src/main/java/pt/uminho/haslab/safecloudclient/clients/DefaultHBaseClient.java
@@ -41,9 +41,8 @@ public class DefaultHBaseClient implements TestClient {
 		Configuration conf = new Configuration();
 		conf.addResource("def-hbase-client.xml");
 		// return new HTable(conf, tableName);
-		CryptoTable ct = new CryptoTable(conf, tableName,
-				CryptoTechnique.CryptoType.DET);
-		ct.cryptoProperties.setKey(readKeyFromFile("key.txt"));
+		CryptoTable ct = new CryptoTable(conf, tableName, CryptoTechnique.CryptoType.STD);
+//		ct.cryptoProperties.setKey(readKeyFromFile("key.txt"));
 		return ct;
 	}
 
